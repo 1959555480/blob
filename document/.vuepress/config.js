b42782f3f22a861bf89b25f6251b4e7232c564f4
yyx <1959555480@qq.com>
2020-06-17 01:06:56 +0800

    博客基本目录建立

--- document/.vuepress/config.js
+++ document/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   title: 'yyx的博客',
-  description: '专注 Node.js 技术栈分享，从前端到Node.js再到数据库',
+  description: '想避世更.要在世上.',
   themeConfig: {
     nav: [
       { text: "主页", link: "/" },
