9b3fa4c7206d4c402066aa44d9d522ccfc6d716b
yyx <1959555480@qq.com>
2020-07-12 09:15:57 +0800

    blob

--- document/.vuepress/config.js
+++ document/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
       { text: "主页", link: "/" },
       {
         text: "前端web",
-        items:[
+        items: [
           {
             text: 'HTML',
             link: "/web/html/"
@@ -27,7 +27,7 @@ module.exports = {
       },
       {
         text: "后端node",
-        items:[
+        items: [
           {
             text: 'egg',
             link: "/node/egg/"
@@ -40,7 +40,7 @@ module.exports = {
       },
       {
         text: "计算机网络",
-        items:[
+        items: [
           {
             text: 'HTTP/TCP',
             link: "/computer/compute/"
@@ -49,7 +49,7 @@ module.exports = {
       },
       {
         text: "面试",
-        items:[
+        items: [
           {
             text: 'HTML 汇总',
             link: "/interview/html/基础"
@@ -72,33 +72,52 @@ module.exports = {
           },
         ]
       },
+      {
+        text: "其他",
+        items: [
+          {
+            text: '正则',
+            link: "/other/reg/base/基础"
+          },
+          {
+            text: '工具网站',
+            link: "/other/website/网站"
+          },
+        ]
+      }
     ],
     sidebar: {
-      '/web/vue/':[
+      '/web/vue/': [
         '',
         'v-model',
         'Vue-router',
         'Vue-vuex',
       ],
-      '/interview/vue/':[
+      '/interview/vue/': [
         '概念',
         'API',
         '原理',
       ],
-      '/interview/js/':[
+      '/interview/js/': [
         '基础',
         'ES6',
         '异步',
       ],
-      '/interview/html/':[
+      '/interview/html/': [
         '基础',
         '扩展',
       ],
-      '/interview/css/':[
+      '/interview/css/': [
         '基础',
       ],
-      '/interview/other/':[
+      '/interview/other/': [
         '浏览器',
+      ],
+      '/other/reg/base/': [
+        '基础',
+      ],
+      '/other/website/': [
+        '网站',
       ]
     },
   },
