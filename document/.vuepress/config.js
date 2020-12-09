d5a35338f551792246f9495979a48411a23c99e6
yyx <1959555480@qq.com>
2020-12-09 18:56:19 +0800

    deploy

--- document/.vuepress/config.js
+++ document/.vuepress/config.js
@@ -8,14 +8,14 @@ module.exports = {
             "script",
             {},
             `
-      var _hmt = _hmt || [];
-      (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?44212d6ce872df50b804d94b24889284";
-        var s = document.getElementsByTagName("script")[0];
-        s.parentNode.insertBefore(hm, s);
-      })();
-        `
+            var _hmt = _hmt || [];
+            (function() {
+              var hm = document.createElement("script");
+              hm.src = "https://hm.baidu.com/hm.js?8deefeec7a588c43f8270cc3900ac0db";
+              var s = document.getElementsByTagName("script")[0]; 
+              s.parentNode.insertBefore(hm, s);
+            })();
+            `
         ]
     ],
     themeConfig: {
