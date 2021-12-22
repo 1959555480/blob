b30acfb17888dd7fef28fbfbe00586d2713e2b93
yanyongxing <1959555480@qq.com>
2021-12-22 11:07:19 +0800

    fix:bug

--- play.sh
+++ play.sh
@@ -1,16 +1,18 @@
-# !/usr/bin/env sh  需要在服务器上执行此脚本
-
-# 确保脚本抛出遇到的错误
-# set -e
-
 # 生成静态文件
 git pull
 
 npm run build
 echo "npm run build"
 
-docker build -t  yyx-blob:latest .
-echo "deploy finish!!!"
+
+# 打包镜像
+# docker build -t yyx-blob:latest .
+# 推镜像
+docker push reg.htres.cn/yw_18kn1/demonginx
+# 展示镜像
+docker images build -t  yyx-blob:latest .
+# 运行镜像
+
 # 进入生成的文件夹
 #cd document/.vuepress/dist
 
