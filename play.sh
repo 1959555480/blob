ee576fbd1e740a1713b21456e7faa66edc35d787
yanyongxing <1959555480@qq.com>
2021-12-22 15:04:29 +0800

    fix;

--- play.sh
+++ play.sh
@@ -7,10 +7,10 @@ echo "npm run build"
 
 # 打包镜像
 echo '开始执行打包镜像'
-docker build -t yyx-blob:1.0 .
+docker build -t yyx-blob .
 echo '停止旧镜像删除'
-docker stop yyx-blob:1.0
-docker rm yyx-blob:1.0
+docker stop yyx-blob
+docker rm yyx-blob
 echo '启动新镜像'
 # 运行镜像
-docker container run -p 80:80 --name yyx-blob:1.0 -d yyx-blob:1.0
+docker container run -p 80:80 --name yyx-blob -d yyx-blob
