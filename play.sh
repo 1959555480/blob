b99ce3a8076f8ac369d749a2cd36eab9df172969
yanyongxing <1959555480@qq.com>
2021-12-22 14:54:04 +0800

    fix;

--- play.sh
+++ play.sh
@@ -12,21 +12,5 @@ echo '停止旧镜像删除'
 docker stop yyx-blob:1.0
 docker rm yyx-blob:1.0
 echo '启动新镜像'
-# 展示镜像
-docker container run -p 80:80 --name yyx-blob -d yyx-blob:1.0
 # 运行镜像
-
-# 进入生成的文件夹
-#cd document/.vuepress/dist
-
-# git init
-# git add -A
-# git commit -m 'deploy'
-
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:1959555480/1959555480.github.io.git master
-
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
-# cd -
-
+docker container run -p 80:80 --name yyx-blob:1.0 -d yyx-blob:1.0
